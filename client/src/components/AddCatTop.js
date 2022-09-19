1b72f05b295e8ba30b00f8c42a7f0e1a700f903a
cpaschall <cpaschall216@gmail.com>
2022-09-19 05:39:21 -0400

    creating Modal form

---
+++ client/src/components/AddCatTop.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Modal, Container, Button, Row, Col }from 'react-bootstrap';
+
+export default function AddCatTop(props) {
+    return (
+        <Modal
+            {...props}
+        >
+
+        </Modal>
+    )
+ }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import AddCatTop from './AddCatTop'
 
 export default function Header() {
+    const [modalShow, setModalShow] = useState(false);
+
+    const handleClose = () => setModalShow(false);
+    const handleShow = () => setModalShow(true);
+
     return (
         <div id="welcome">
             <h1>Let's Learn Web Development Together!</h1>
+            <Button variant="primary" onClick={handleShow}>Create</Button>
+            <AddCatTop
+                show={modalShow}
+                onHide={handleClose}
+            />
             {/* <h3>Where would you like to begin?</h3> */}
         </div>
     )
