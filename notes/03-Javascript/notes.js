a9393d6374dfcfd428943b913a3e3eb58c2c6ffe
cpaschall <cpaschall216@gmail.com>
2022-10-31 06:54:17 -0400

    adding notes directory

---
+++ notes/03-Javascript/notes.js
@@ -0,0 +1,57 @@
+// 01 - Script Console Log
+
+// console.log writes code to the terminal or browser's console
+console.log("I'm sending you a message in the console!")
+// console.table creates a table in the console
+// This can be done by creating an array that relates each value to an index position(starting with 0)
+console.table(["value 1", "value 2", "value 3"])
+//  or a table be created as key:value pairs
+console.table({"col 1": "value 1", "col 2": "value 2", "col 3": "value 3"})
+
+//  ------------------------------------------------------------------ //
+
+// 03 - Hello Variable
+
+// the 'var' (variable) keyword is used to bind a value to a designated name
+var myName = "Cal"
+var myAge = 38
+console.log("My name is " + myName + " and I am " + myAge + " years old.") 
+// variable assigments can be changed after they are declared
+var myName = "James"
+var myAge = 22
+console.log("My name is " + myName + " and I am " + myAge + " years old.") 
+
+//  ------------------------------------------------------------------ //
+
+// 05 - Primitive Types
+
+// Primitive data types include undefined, string, number and boolean
+// Undefined are variables that have not been defined
+console.log("Undefined variable:")
+var myUndefined
+console.log(myUndefined)
+// Number variables can be integers or decimals(floats)
+console.log("Number variables:")
+var myInt = 23
+var myFloat = 45.97
+console.log("Integer: " + myInt)
+console.log("Float: " + myFloat)
+// Booleans have two values: true and false
+var isBooleanTrue = true
+var isBooleanFalse = false
+console.log("TRUE/FALSE: The sky is blue: " + isBooleanTrue)
+console.log("TRUE/FALSE: Pigs can fly: " + isBooleanTrue)
+
+//  ------------------------------------------------------------------ //
+
+//  ------------------------------------------------------------------ //
+
+//  ------------------------------------------------------------------ //
+
+//  ------------------------------------------------------------------ //
+
+//  ------------------------------------------------------------------ //
+
+//  ------------------------------------------------------------------ //
+
+//  ------------------------------------------------------------------ //
